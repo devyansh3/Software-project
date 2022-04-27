4ec7f47a811255aa9f10f52a255884a5129a44c7
bhavenjain <bhavenjain@users.noreply.github.com>
2022-04-28 02:22:02 +0530

    votes

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Table} from 'react-bootstrap'
 
+let data = {
+  1: 0,
+  2: 0,
+  3: 0
+}
 
 function List() {
+
+  const vote = (e) => {
+    data[e.target.value] += 1
+  }
+  
   return (
     <div>
       <h1 style={{textAlign:'center', marginTop: '1%'}}>ACM Elections</h1>
@@ -17,24 +27,25 @@ function List() {
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>1</td>
+    <tr  id={"1"}>
+      <td id={"1"}>1</td>
       <td>Mark</td>
       <td>President</td>
-      <td>Vote Confirm</td>
+      <td id="1"><button value="1" id="1" onClick={vote} >Vote Confirm</button></td>
     </tr>
-    <tr>
+    <tr id={"2"}>
       <td>2</td>
       <td>Jacob</td>
       <td>President</td>
-      <td>Vote Confirm</td>
+      <td id = '2'><button value="2" id="2" onClick={vote}>Vote Confirm</button>
+</td>
       
     </tr>
-    <tr>
+    <tr id={"3"}>
       <td>3</td>
       <td>Larry</td>
       <td>Secretary</td>
-      <td>Vote Confirm</td>
+      <td id="3"><button value="3" id="3" onClick = { vote }>Vote Confirm</button></td>
 
       
     </tr>
